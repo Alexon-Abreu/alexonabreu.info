23a29fa88050e98b957a97d8c377f491904de05e
Alexon-Abreu <alexonabreuramirez@gmail.com>
2025-01-01 17:43:54 -0500

    updating resume

--- src/portfolio.js
+++ src/portfolio.js
@@ -10,7 +10,7 @@ const about = {
   role: 'Full-Stack Engineer',
   description:
     'From a young age, I’ve been passionate about technology. It’s given me a way to explore new ideas and express my creativity through software development. Now, as I near the end of my bachelor’s degree in Computer Science, I’m focused on building innovative software with a great team to make a meaningful impact.',
-  resume: 'https://drive.google.com/file/d/1RhrnE7RT5sW50HQRA53SL-jM6kPPCfVO/view?usp=sharing',
+  resume: 'https://drive.google.com/file/d/1VjVvhScwzZFEGOAWyUNuJwiOtsN220K1/view?usp=sharing',
   social: {
     linkedin: 'https://www.linkedin.com/in/alexon-abreu/',
     github: 'https://github.com/Alexon-Abreu',
