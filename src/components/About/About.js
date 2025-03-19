d070de144401a79d8ecd683d7b762551e8060746
Alexon-Abreu <alexonabreuramirez@gmail.com>
2025-03-18 23:09:32 -0400

    newTab func Resume/GitHub/Linkedin

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -15,12 +15,15 @@ const About = () => {
       )}
       
       {role && <h2 className='about__role'>{role}</h2>}
-      {/* {role && <h2 className='about__role'>A {role}.</h2>} */}
       <p className='about__desc'>{description && description}</p>
 
       <div className='about__contact center'>
         {resume && (
-          <a href={resume}>
+          <a 
+            href={resume} 
+            target="_blank" 
+            rel="noopener noreferrer"
+          >
             <span type='button' className='btn btn--outline'>
               Resume
             </span>
@@ -34,6 +37,8 @@ const About = () => {
                 href={social.github}
                 aria-label='github'
                 className='link link--icon'
+                target="_blank"
+                rel="noopener noreferrer"
               >
                 <GitHubIcon />
               </a>
@@ -44,6 +49,8 @@ const About = () => {
                 href={social.linkedin}
                 aria-label='linkedin'
                 className='link link--icon'
+                target="_blank"
+                rel="noopener noreferrer"
               >
                 <LinkedInIcon />
               </a>
